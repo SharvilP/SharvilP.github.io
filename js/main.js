770f2dac4d384c2a926538f2d9dce2be6fba8795
Sharvil <sparekh@whiskerlabs.com>
2017-07-10 14:54:46 -0400

    add cardr

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function changeToHammock() {
+    $("#profileImg").attr('src', 'images/hammock.png');
+}
+
+function changeToHike() {
+    $("#profileImg").attr('src', 'images/hiking.png');
+}
+
+function changeToNormal() {
+    $("#profileImg").attr('src', 'images/profile.jpg');
+}
